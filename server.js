a5fcbe8deab1b06da4bd0ac423ae53433b043d9d
root <root@DESKTOP-HM3SMEL.localdomain>
2020-12-19 00:11:14 -0500

    add redirect to fallin release

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 
 const port = process.env.PORT || 8080;
@@ -6,10 +6,13 @@ const port = process.env.PORT || 8080;
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-
 //templating here
 app.get('/', (req, res) => {
     res.render('pages/index')
+})
+
+app.get('/release/fallin',  (req, res) => {
+    res.redirect('https://distrokid.com/hyperfollow/slan/fallin')
 })
 
 app.listen(port, () => console.log('listening on port 8080'))
